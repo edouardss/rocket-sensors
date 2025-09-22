9534f0d827bcea652839499420b98315acbf6c04
Edouard Servan-Schreiber <edouardss@gmail.com>
2025-09-22 17:24:40 -0400

    first commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import asyncio
+from viam.module.module import Module
+try:
+    from models.mpu import Mpu
+except ModuleNotFoundError:
+    # when running as local module with run.sh
+    from .models.mpu import Mpu
+
+
+if __name__ == '__main__':
+    asyncio.run(Module.run_from_registry())
